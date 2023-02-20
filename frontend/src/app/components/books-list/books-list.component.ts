4f9c6dbe3a2e2481fad2778f82909e66c803bb16
Taavi Kamarik <90038572+TaaviKamarik@users.noreply.github.com>
2023-02-20 09:00:42 +0200

    Implemented books table, sorting and pagination.

--- frontend/src/app/components/books-list/books-list.component.ts
+++ frontend/src/app/components/books-list/books-list.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { BookService } from '../../services/book.service';
-import { Observable } from 'rxjs';
-import { Page } from '../../models/page';
-import { Book } from '../../models/book';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {BookService} from '../../services/book.service';
+import {Observable} from 'rxjs';
+import {Page} from '../../models/page';
+import {Book} from '../../models/book';
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-books-list',
@@ -12,6 +15,12 @@ import { Book } from '../../models/book';
 export class BooksListComponent implements OnInit {
 
   books$: Observable<Page<Book> | Error>;
+  bookList = new MatTableDataSource();
+  columns: string[] = ["title", "author", "year", "status"]
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
 
   constructor(
     private bookService: BookService,
@@ -21,7 +30,15 @@ export class BooksListComponent implements OnInit {
   ngOnInit(): void {
     // TODO this observable should emit books taking into consideration pagination, sorting and filtering options.
     this.books$ = this.bookService.getBooks({});
+    this.books$.subscribe((result) => {
+      console.log(result.content)
+      this.bookList.data = result.content;
+    });
+  }
 
+  ngAfterViewInit() {
+    this.bookList.paginator = this.paginator;
+    this.bookList.sort = this.sort;
   }
 
 }
