f157484ae1a6e32d5d035d1ff00eefa61889998b
Taavi Kamarik <90038572+TaaviKamarik@users.noreply.github.com>
2023-02-20 10:30:25 +0200

    Implemented checkout table, sorting and pagination.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { BooksListComponent } from './components/books-list/books-list.component';
-import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BooksListComponent} from './components/books-list/books-list.component';
+import {BookDetailComponent} from './components/book-detail/book-detail.component';
+import {CheckoutsListComponent} from "./components/checkouts-list/checkouts-list.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'books', pathMatch: 'full'},
+  {path: 'checkouts', redirectTo: 'checkouts', pathMatch: 'full'},
   {path: 'books', component: BooksListComponent},
+  {path: 'checkouts', component: CheckoutsListComponent},
   {path: 'books/:id', component: BookDetailComponent}
 ];
 
